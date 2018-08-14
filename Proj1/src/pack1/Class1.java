6a6dd02455acb74b5983cd0c1e76c3325f1a693b
aquariusnawab <42366965+aquariusnawab@users.noreply.github.com>
2018-08-14 01:53:15 -0600

    Update Class1.java

--- Proj1/src/pack1/Class1.java
+++ Proj1/src/pack1/Class1.java
@@ -15,6 +15,7 @@ public class Class1 {
 			Thread.sleep(3000);
 			w.quit();
 			System.out.println("git");
+			System.out.println("git");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
