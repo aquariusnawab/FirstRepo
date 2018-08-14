153f7eea52141f384c41f8c42b794828133a61d7
206527398 <206527398@potapws00031ld.stg-tfayd.com>
2018-08-14 00:24:42 -0600

    First Commit

---
+++ Proj1/src/pack1/Class1.java
@@ -0,0 +1,51 @@
+package pack1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Class1 {
+	public static void main(String[] args) {
+		try {
+			System.out.println("First Project");
+
+			System.setProperty("webdriver.chrome.driver", "D:\\GTM Full\\JARS\\ChromeLatestDriver\\chromedriver.exe");
+			WebDriver w = new ChromeDriver();
+			w.get("https://google.com");
+			Thread.sleep(3000);
+			w.quit();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Test
+	public void tc1() {
+		try {
+			System.out.println("TestNG TC1");
+
+			System.setProperty("webdriver.chrome.driver", "D:\\GTM Full\\JARS\\ChromeLatestDriver\\chromedriver.exe");
+			WebDriver w = new ChromeDriver();
+			w.get("https://google.com");
+			Thread.sleep(3000);
+			w.quit();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Test
+	public void tc2() {
+		try {
+			System.out.println("TestNG TC1");
+
+			System.setProperty("webdriver.chrome.driver", "D:\\GTM Full\\JARS\\ChromeLatestDriver\\chromedriver.exe");
+			WebDriver w = new ChromeDriver();
+			w.get("https://google.com");
+			Thread.sleep(3000);
+			w.quit();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
