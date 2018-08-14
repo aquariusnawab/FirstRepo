01d67244be9bea319692e8a391c797a29ac02aa1
206527398 <206527398@potapws00031ld.stg-tfayd.com>
2018-08-14 02:17:22 -0600

    third commit

--- Proj1/src/pack1/Class1.java
+++ Proj1/src/pack1/Class1.java
@@ -15,7 +15,7 @@ public class Class1 {
 			Thread.sleep(3000);
 			w.quit();
 			System.out.println("git");
-			System.out.println("git12suces");
+			System.out.println("git12suces123");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
